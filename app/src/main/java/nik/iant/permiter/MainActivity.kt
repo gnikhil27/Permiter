b79b6970d27812114d635806d1a7381e9ce3a191
none <none@none.none>
2021-05-12 12:10:28 +0530

    Initial commit

---
+++ app/src/main/java/nik/iant/permiter/MainActivity.kt
@@ -0,0 +1,48 @@
+package nik.iant.permiter
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnCaller = findViewById<Button>(R.id.btnCall)
+        btnCaller.setOnClickListener{
+            try{
+
+                //Check if i have Permission
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                {
+                    // Ask for the Permission
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 0)
+                    //Check if i was given permission
+                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                        callMe()
+                    else findViewById<TextView>(R.id.txtlog).text = "Sorry, We Require permission to call"
+                }
+                else // if I have permission Call the Number
+                    callMe()
+            }catch (Ex:Exception)
+            {
+                findViewById<TextView>(R.id.txtlog).text = Ex.message
+            }
+        }
+    }
+
+    fun callMe()
+    {
+        val callPage = Intent(Intent.ACTION_CALL)
+        callPage.data = Uri.parse("tel:919998887650")
+        startActivity(callPage)
+    }
+}
