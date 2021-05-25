b70e22ce62888706e7ea7a60c62465077e097146
none <none@none.none>
2021-05-25 12:05:12 +0530

    Added Some RecyclerView Front-End

--- app/src/main/java/nik/iant/permiter/VegAdapter.kt
+++ app/src/main/java/nik/iant/permiter/VegAdapter.kt
@@ -1,4 +1,33 @@
 package nik.iant.permiter
 
-class VegAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class VegAdapter(private val nContext: Context, private val lstVeg: Array<String>):RecyclerView.Adapter<VegAdapter. MyViewHolder>() {
+
+    class MyViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        fun setCheckBox(string: String){
+            val checkBox = itemView.findViewById<CheckBox>(R.id.txtFoodItem)
+            checkBox.text = string
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.item_vegitables, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        Toast.makeText(nContext, "Called", Toast.LENGTH_SHORT).show()
+        holder.setCheckBox(lstVeg[position])
+    }
+
+    override fun getItemCount(): Int {
+        return lstVeg.count()
+    }
 }
--- app/src/main/java/nik/iant/permiter/showList.kt
+++ app/src/main/java/nik/iant/permiter/showList.kt
@@ -2,6 +2,8 @@ package nik.iant.permiter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class showList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,13 @@ class showList : AppCompatActivity() {
         setContentView(R.layout.activity_show_list)
         title = "Please Select your Food"
         setTheme(android.R.style.Theme_DeviceDefault_Light_NoActionBar)
+
+        val myRecycler = findViewById<RecyclerView>(R.id.rcvVegies)
+        val lmm = LinearLayoutManager(this)
+        val myAdapter = VegAdapter(this, arrayOf(
+            "Tomato","Potato","Brinjal"
+        ))
+        myRecycler.layoutManager = lmm
+        myRecycler.adapter = myAdapter
     }
 }
